bf4d2482bdb359bda37684808d2f0b9d11595614
Anthea Ip <antheaip964@gmail.com>
2021-10-06 20:04:11 -0400

    Adding Choc model

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ console.log(process.env.PORT);
 //Dependencies
 //___________________
 const express = require('express');
+const morgan = require("morgan") //import morgan
 const methodOverride = require('method-override');
 const mongoose = require ('mongoose');
-const app = express();
+
+// Not in Superfruits "complete server.js file"
 const db = mongoose.connection;
 //___________________
 //Port
@@ -16,13 +18,22 @@ const PORT = process.env.PORT || 3000;
 
 //___________________
 //Database
+
+// Setup inputs for our connect function; added from SuperFruits
+const DATABASE_URL = process.env.DATABASE_URL
+const CONFIG = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+
 //___________________
-// How to connect to the database either via heroku or locally
+// How to connect to the database either via heroku or locally; from Heroku Lab
 const MONGODB_URI = process.env.MONGODB_URI;
 
 // Connect to Mongo &
 // Fix Depreciation Warnings from Mongoose
 // May or may not need these depending on your Mongoose version
+// From Heroku Lab
 mongoose.connect(MONGODB_URI , { useNewUrlParser: true, useUnifiedTopology: true}
 );
 
@@ -31,19 +42,42 @@ db.on('error', (err) => console.log(err.message + ' is mongod not running?'));
 db.on('connected', () => console.log('mongod connected: ', MONGODB_URI));
 db.on('disconnected', () => console.log('mongod disconnected'));
 
+////////////////////////////////////////////////
+// Our Models
+////////////////////////////////////////////////
+// pull schema and model from mongoose
+const {Schema, model} = mongoose
+
+// make chocolate schema
+const chocoSchema = new Schema({
+    Manuf: String,
+    Name: String,
+    CocoaCont: Number,
+    Flavor: String,
+    Description: String
+})
+
+// make choc model
+const Choc = model("Choc", chocoSchema)
+
+/////////////////////////////////////////////////
+// Create our Express Application Object, from Superfruits
+/////////////////////////////////////////////////
+const app = express()
+
 //___________________
 //Middleware
 //___________________
+app.use(morgan("tiny")) //logging
+//use method override
+app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
 
-//use public folder for static assets
-app.use(express.static('public'));
+//use public folder for static assets; not in Superfruits Register Middleware
+// app.use(express.static('public'));
 
 // populates req.body with parsed info from forms - if no data from forms will return an empty object {}
 app.use(express.urlencoded({ extended: false }));// extended: false - does not allow nested objects in query strings; gets req.body
 app.use(express.json());// returns middleware that only parses JSON - may or may not need it depending on your project
-
-//use method override
-app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
 
 
 //___________________
@@ -53,6 +87,13 @@ app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
 app.get('/' , (req, res) => {
   res.send('Hello World!');
 });
+
+
+
+
+
+
+
 
 //___________________
 //Listener
